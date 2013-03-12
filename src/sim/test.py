416480cafea3bea0262dca3b5dfdf2374e99c83b
dmlb2000 <dmlb2000@cdfc2803-16d5-4e1d-b392-7172f0f6df19>
2013-03-12 21:30:54 +0000

    add some test code to pull out xpath info from cml
    git-svn-id: https://emslhub.emsl.pnl.gov/tools/nmrtools/svn/trunk@36 cdfc2803-16d5-4e1d-b392-7172f0f6df19

--- src/sim/test.py
+++ src/sim/test.py
@@ -6,6 +6,29 @@ import sys
 xmlstr = sys.stdin.read()
 doc = libxml2.parseMemory(xmlstr, len(xmlstr))
 ctxt = doc.xpathNewContext()
-for node in ctxt.xpathEval("/*"):
-	print dir(node)
-	print node.name
+ctxt.xpathRegisterNs('cml', "http://www.xml-cml.org/schema")
+ATOM="O1.2"
+xpath = {
+	"EFG":
+	"//cml:propertyList[@ref = \"%s\"]//cml:property[@dictRef = \"nwchem:efgPrincipalComponents\"]"%(ATOM),
+	"shielding":
+	"//cml:propertyList[@ref = \"%s\"]//cml:property[@dictRef = \"nwchem:shieldingPrincipalComponents\"]"%(ATOM),
+	"EulerAlpha":
+	"//cml:propertyList[@ref = \"%s\"]//cml:property[@dictRef = \"nwchem:eulerAngleAlpha\"]"%(ATOM),
+	"EulerBeta":
+	"//cml:propertyList[@ref = \"%s\"]//cml:property[@dictRef = \"nwchem:eulerAngleBeta\"]"%(ATOM),
+	"EulerGamma":
+	"//cml:propertyList[@ref = \"%s\"]//cml:property[@dictRef = \"nwchem:eulerAngleGamma\"]"%(ATOM),
+}
+values = {}
+for k in xpath:
+	x = xpath[k]
+	for node in ctxt.xpathEval(x):
+		values[k] = node.getContent().strip()
+		print x, k
+		print node.get_properties()
+		print node.getContent()
+
+print values['shielding']
+print values['EulerAlpha'], values['EulerBeta'], values['EulerGamma']
+print "-0.02578e-24", [ "%e"%(float(a)*3.24136E15) for a in values['EFG'].split() ]
