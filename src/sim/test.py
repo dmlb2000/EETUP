50ae008fe91385104d86464c9e21a4354124696d
dmlb2000 <dmlb2000@cdfc2803-16d5-4e1d-b392-7172f0f6df19>
2013-03-12 19:52:31 +0000

    add a test python program
    git-svn-id: https://emslhub.emsl.pnl.gov/tools/nmrtools/svn/trunk@33 cdfc2803-16d5-4e1d-b392-7172f0f6df19

---
+++ src/sim/test.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+import libxml2
+import sys
+
+xmlstr = sys.stdin.read()
+doc = libxml2.parseMemory(xmlstr, len(xmlstr))
+ctxt = doc.xpathNewContext()
+for node in ctxt.xpathEval("/*"):
+	print dir(node)
+	print node.name
