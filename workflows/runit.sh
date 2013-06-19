23dadc11c1fd7e089738ebeccde74a3a176d9fa1
dmlb2000 <dmlb2000@cdfc2803-16d5-4e1d-b392-7172f0f6df19>
2013-06-19 15:01:36 +0000

    add some more bits
    git-svn-id: https://emslhub.emsl.pnl.gov/tools/nmrtools/svn/trunk@57 cdfc2803-16d5-4e1d-b392-7172f0f6df19

--- workflows/runit.sh
+++ workflows/runit.sh
@@ -8,7 +8,9 @@ kepler -runkar \
 	-username "$USERNAME" \
 	-password "$PASSWORD" \
 	-query 'proposal=45796#NWChem.CML=test_apr_17' \
-	-upmetadata 'proposal=45796#Tag=nmrpipe_test_may_16' \
+	-expquery 'proposal=45796#Tag=NMR_EXP_jun_19' \
+	-upmetadata 'proposal=45796#Tag=nmrpipe_test_jun_19' \
 	-simloc /home/dmlb2000/nmrtools/trunk/src/sim/Quad_NMR_Simulator \
 	-isoloc /home/dmlb2000/nmrtools/trunk/src/sim/oxygen17.inp.txt \
+	-conv_nmrpipe_cmd 'bin2pipe -bo 1056 -xN 256 -xT 128 -xMODE Complex -noswap'
 	$PWD/NWChemNMR.kar
